76865ea642313a971152631c6e48179748c861ad
Zakaria_Kasmi <zkasmi@inf.fu-berlin.de>
2013-06-28 15:49:25 +0200

    definitions of the driver for the SRF02 ultrasonic sensor

--- msba2-common/drivers/include/srf02-ultrasonic-sensor.h
+++ msba2-common/drivers/include/srf02-ultrasonic-sensor.h
@@ -25,39 +25,39 @@
 
 
 /* define the SRF02 registers*/
-#define SRF02_DEFAULT_ADDR 			112
-#define SRF02_COMMAND_REG			0x0
+#define SRF02_DEFAULT_ADDR 		112
+#define SRF02_COMMAND_REG		0x0
 #define SRF02_RANGE_HIGH_BYTE		0x2
 #define SRF02_RANGE_LOW_BYTE		0x3
 #define SRF02_REAL_RANGING_MODE_CM	0x51
 
 
 /* Define the used I2C Interface */
-//#define SRF02_I2C_INTERFACE I2C0		// P0.27 SDA0, P0.28 SCL0
-//#define SRF02_I2C_INTERFACE I2C1_0		// P0.0  SDA1, P0.1  SCL1
-//#define SRF02_I2C_INTERFACE I2C1_1		// P0.19 SDA1, P0.20 SCL1
+//#define SRF02_I2C_INTERFACE I2C0	// P0.27 SDA0, P0.28 SCL0
+//#define SRF02_I2C_INTERFACE I2C1_0	// P0.0  SDA1, P0.1  SCL1
+//#define SRF02_I2C_INTERFACE I2C1_1	// P0.19 SDA1, P0.20 SCL1
 #define SRF02_I2C_INTERFACE I2C2	// P0.10 SDA2, P0.11 SCL2
 
-#define SRF02_EXIT_MSG		0
-#define SRF02_RANGING_MSG 	1
-#define SRF02_SLEEP_MSG		2
-#define SRF02_WEAKUP_MSG	3
+#define SRF02_EXIT_MSG			0
+#define SRF02_RANGING_MSG 		1
+#define SRF02_SLEEP_MSG			2
+#define SRF02_WEAKUP_MSG		3
 
 
 /**
- * @brief	 Initialize the SRF02 ultrasonic sensor.
+ * @brief	Initialize the SRF02 ultrasonic sensor.
  *
- * @param[in] i2c_interface		the i2c interface, several interfaces can be selected: i2c0, i2c1 and i2c2.
- * @param[in] baud_rate			the baud rate.
+ * @param[in] i2c_interface	the i2c interface, several interfaces can be selected: i2c0, i2c1 and i2c2.
+ * @param[in] baud_rate		the baud rate.
  *
- *@return 	true if the SRF02 is successfully initialized, otherwise false.
+ *@return true if the SRF02 is successfully initialized, otherwise false.
  */
 bool srf02_init(uint8_t i2c_interface, uint32_t baud_rate);
 
 
 /**
- * @brief   Start a continuous sampling of the distance measures.
- *			This function prints the distance values in centimeters over the rs232 interface.
+ * @brief	Start a continuous sampling of the distance measures.
+ *		This function prints the distance values in centimeters over the rs232 interface.
  *
  */
 void srf02_start_ranging(void);
