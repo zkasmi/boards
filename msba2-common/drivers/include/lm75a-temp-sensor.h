4fb342bfc2b11bfae0adaa2073135c9e5f3a0fda
Zakaria_Kasmi <zkasmi@inf.fu-berlin.de>
2013-06-28 15:45:47 +0200

    definitions of the driver for the LM75A temperature sensor

--- msba2-common/drivers/include/lm75a-temp-sensor.h
+++ msba2-common/drivers/include/lm75a-temp-sensor.h
@@ -26,17 +26,17 @@
 #include "i2c.h"
 
 /* LM75A register addresses */
-#define LM75A_ADDR 				0x48
-#define LM75A_TEMPERATURE_REG  	0x0
+#define LM75A_ADDR 			0x48
+#define LM75A_TEMPERATURE_REG  		0x0
 #define LM75A_CONFIG_REG 		0x1
 #define LM75A_THYST_REG			0x2
 #define LM75A_OVER_TEMP_REG		0x3
 
 /* Define the used I2C Interface */
-//#define LM75A_I2C_INTERFACE I2C0					// P0.27 SDA0, P0.28 SCL0
-#define LM75A_I2C_INTERFACE 			I2C1_0		// P0.0  SDA1, P0.1  SCL1
-//#define LM75A_I2C_INTERFACE I2C1_1				// P0.19 SDA1, P0.20 SCL1
-//#define LM75A_I2C_INTERFACE I2C2					// P0.10 SDA2, P0.11 SCL2
+//#define LM75A_I2C_INTERFACE I2C0			// P0.27 SDA0, P0.28 SCL0
+#define LM75A_I2C_INTERFACE 		I2C1_0		// P0.0  SDA1, P0.1  SCL1
+//#define LM75A_I2C_INTERFACE I2C1_1			// P0.19 SDA1, P0.20 SCL1
+//#define LM75A_I2C_INTERFACE I2C2			// P0.10 SDA2, P0.11 SCL2
 
 /* LM75A operation modes */
 enum OPERATION_MODES {
@@ -47,24 +47,24 @@ enum OPERATION_MODES {
 };
 
 /*Common definitions for LMA75A  */
-#define LM75A_BIT0    				0x0
-#define LM75A_BIT1    				0x1
-#define LM75A_BIT2   				0x2
-#define LM75A_BIT3    				0x3
-#define LM75A_BIT4    				0x4
-#define LM75A_BIT5   				0x5
-#define LM75A_BIT6    				0x6
-#define LM75A_BIT7    				0x7
-#define LM75A_BIT8					0x8
-#define LM75A_BIT9					0x9
-#define LM75A_BIT10					0xA
-#define LM75A_BIT15					0xF
-#define LM75A_MOST_SIG_BYTE_MASK    0xFF00
+#define LM75A_BIT0    			0x0
+#define LM75A_BIT1    			0x1
+#define LM75A_BIT2   			0x2
+#define LM75A_BIT3    			0x3
+#define LM75A_BIT4    			0x4
+#define LM75A_BIT5   			0x5
+#define LM75A_BIT6    			0x6
+#define LM75A_BIT7    			0x7
+#define LM75A_BIT8			0x8
+#define LM75A_BIT9			0x9
+#define LM75A_BIT10			0xA
+#define LM75A_BIT15			0xF
+#define LM75A_MOST_SIG_BYTE_MASK   	0xFF00
 #define LM75A_LEAST_SIG_BYTE_MASK	0x00FF
 #define LM75A_DATA_BITS_MASK		0x07FF
-#define LM75A_SIGN_BIT_MASK			(1<<LM75A_BIT10)
-#define LM75A_LSB_MASK				0x1
-#define LM75A_EXTINT_MODE			0x1
+#define LM75A_SIGN_BIT_MASK		(1<<LM75A_BIT10)
+#define LM75A_LSB_MASK			0x1
+#define LM75A_EXTINT_MODE		0x1
 
 /* LM75A configuration register */
 #define LM75A_ACTIVE_LOW        	0
@@ -89,13 +89,13 @@ enum DEFAULT_VALUES {
 };
 
 /*define inter-threads messages */
-#define LM75A_EXIT_MSG		0
-#define LM75A_SAMPLING_MSG 	1
-#define LM75A_SLEEP_MSG		2
-#define LM75A_WEAKUP_MSG	3
+#define LM75A_EXIT_MSG			0
+#define LM75A_SAMPLING_MSG 		1
+#define LM75A_SLEEP_MSG			2
+#define LM75A_WEAKUP_MSG		3
 
 /**
- * @brief   Set the over-temperature shutdown threshold (TOS).
+ * @brief	Set the over-temperature shutdown threshold (TOS).
  *
  * @param[in] tos	the TOS value.
  *
@@ -114,8 +114,8 @@ void lm75A_set_hysteresis_temperature(float_t thsyt);
 
 /**
  * @brief   Set various operation modes of the temperature sensor.
- *			The LM75A provide four modes: normal, comparator, interrupt, and the shutdown mode.
- *			All these modes are defined in the lm75a-temp-sensor.h
+ *		The LM75A provide four modes: normal, comparator, interrupt, and the shutdown mode.
+ *		All these modes are defined in the lm75a-temp-sensor.h
  *
  * @param[in]  op_mode	the operation mode value: the normal, shutdown, comparator, or interrupt mode.
  *
@@ -126,7 +126,7 @@ void lm75A_set_operation_mode(uint8_t op_mode);
 /**
  * @brief   Get the content of the configuration register.
  *
- * @return  the configuration register value.
+ * @return the configuration register value.
  *
  */
 uint8_t lm75A_get_config_reg(void);
@@ -144,7 +144,7 @@ float_t lm75A_get_hysteresis_temperature(void);
 /**
  * @brief   Get the adjusted over-temperature shutdown threshold (TOS).
  *
- * @return  the content of the TOS-register.
+ * @return the content of the TOS-register.
  *
  */
 float_t lm75A_get_over_temperature(void);
@@ -160,31 +160,31 @@ float_t lm75A_get_ambient_temperature(void);
 
 
 /**
- * @brief   Set the LM75A sensor in the initial state.
- *			The temperature sensor has the following values in this state:
- *			config_register = 0; hyst_register = 75; the tos_reg = 80.
+ * @brief	Set the LM75A sensor in the initial state.
+ *		The temperature sensor has the following values in this state:
+ *		config_register = 0; hyst_register = 75; the tos_reg = 80.
  *
  */
 void lm75A_reset(void);
 
 
 /**
- * @brief   Start a continuous sampling of the temperature values.
- *			This function prints the values of all registers over the rs232 interface.
+ * @brief	Start a continuous sampling of the temperature values.
+ *		This function prints the values of all registers over the rs232 interface.
  *
- *@param[in] external_interr_handler	pointer to an external task handler which is performed, if an external interrupt is occurred and the external subroutine is leaved.
- *										This parameter is optional, the NULL-value can be entered.
+ *@param[in] external_interr_handler	pointer to an external task handler which is performed, if an external interrupt is occurred and 
+ *					the external subroutine is leaved. This parameter is optional, the NULL-value can be entered.
  */
 void lm75A_start_sensor_sampling(void (*extern_interrupt_task)(void));
 
 
 /**
- * @brief	 Register an interrupt handler for the external interrupt.
- * 			 Only the port0 and port2 are supported.
+ * @brief	Register an interrupt handler for the external interrupt.
+ * 		Only the port0 and port2 are supported.
  *
- * @param[in] port			port number
+ * @param[in] port		port number
  * @param[in] pin_bit_mask 	pin number in form of a bit mask: Pin0 --> BIT0, Pin1 --> BIT1, Pin2 --> BIT2 = 2^2 = 4
- * @param[in] flags			define if the interrupt is generated on rising or falling edge (#GPIOINT_RISING_EDGE, #GPIOINT_FALLING_EDGE)
+ * @param[in] flags		define if the interrupt is generated on rising or falling edge (#GPIOINT_RISING_EDGE, #GPIOINT_FALLING_EDGE)
  * @param[in] handler		pointer to an interrupt handler.
  *
  * @return true if the the external interrupt handler is successfully registered, otherwise false.
@@ -194,13 +194,13 @@ bool lm75A_ext_irq_handler_register(int32_t port, uint32_t pin_bit_mask,
 
 
 /**
- * @brief	 Initialize the LM75A temperature sensor.
- * 			 The baud rate and the handler for the external interrupt can be initialized. The external interrupt is optional,
- * 			 if no handler is available, the NULL-value can be entered. The hysteresis and the over-temperature are displayed before and after a rest. After this the LM7A sensor
- * 			 is set in the interrupt or the comparator mode.
+ * @brief	Initialize the LM75A temperature sensor.
+ * 		The baud rate and the handler for the external interrupt can be initialized. The external interrupt handler is optional,
+ * 		if no handler is available, the NULL-value can be entered. The hysteresis and the over-temperature are displayed 
+ * 		before and after a rest action is performed. After this the LM7A sensoris set in the interrupt or the comparator mode.
  *
- * @param[in] i2c_interface				the i2c interface, several interfaces can be selected: i2c0, i2c1 and i2c2.
- * @param[in] baud_rate					the baud rate.
+ * @param[in] i2c_interface		the i2c interface, several interfaces can be selected: i2c0, i2c1 and i2c2.
+ * @param[in] baud_rate			the baud rate.
  * @param[in] external_interr_handler	pointer to a handler for the external interrupt.
  *
  * @return true if the I2C interface and the external interrupt handler are successfully initialized, otherwise false.
@@ -210,19 +210,19 @@ bool lm75A_init(uint8_t i2c_interface, uint32_t baud_rate,
 
 
 /**
- * @brief	 Register the external interrupt handler for the over-temperature shutdown output.
+ * @brief	Register the external interrupt handler for the over-temperature shutdown output.
  *
- * @param[in]	handler	pointer to a handler for the external interrupts.
+ * @param[in] handler	pointer to a handler for the external interrupts.
  *
- * @return 	true if the the external interrupt handler is successfully registered, otherwise false.
+ * @return true if the the external interrupt handler is successfully registered, otherwise false.
  */
 bool lm75A_external_interrupt_register(void* handler);
 
 
 /**
- * @brief	 Alarm the sensor sampling task about an external interrupt.
+ * @brief	Alarm the sensor sampling task about an external interrupt.
  *
- * @param[in]	b is true if an external interrupt is occurred, otherwise false.
+ * @param[in] b	is true if an external interrupt is occurred, otherwise false.
  *
  */
 void lm75A_set_in_alarm(bool b);
